e5b7822b488a48f9357a7db5eb8d7d39490b9660
ouioui25 <qp5minute@gmail.com>
2021-08-25 20:17:42 +0900

    station4

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // DO NOT DELETE
 
 import * as React from 'react'
+import React, { useState } from 'react';
 import './App.css'
 
 /**
@@ -8,9 +9,17 @@ import './App.css'
  * @type {React.FC}
  */
 export const App = () => {
+    const [dogUrl] = useState('ja.reactjs.org/docs/introducing-jsx.html#embedding-expressions-in-jsx');
   return (
+    <>
+    <header>
+    </header>
     <div>
       <h2>Hello, world!</h2>
+      
+      <img src ={dogUrl}></img>
     </div>
+    </>
   )
+
 }
